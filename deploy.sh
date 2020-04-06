505a5ad6e7a26fb821029ae051ae448ee051ce52
Dan Perron <github@djperron.com>
2020-04-05 22:12:13 -0500

    Automatically restart

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker build -t dan-perron/webhook-server .
 docker stop webhook-server
 docker rm webhook-server
-docker run  --name webhook-server -p 3000:3000 -d dan-perron/webhook-server
+docker run  --name webhook-server --restart unless-stopped -p 3000:3000 -d dan-perron/webhook-server
