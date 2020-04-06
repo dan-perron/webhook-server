9c4438a9bf750fdbc699e3ebacc649929c202e1e
Dan Perron <github@djperron.com>
2020-04-06 17:43:05 -0500

    Add post deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 docker build -t dan-perron/webhook-server .
 docker stop webhook-server
 docker rm webhook-server
 docker run  --name webhook-server --restart unless-stopped -p 3000:3000 -d dan-perron/webhook-server
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Deploy complete"}' http://localhost/webhooks/test
