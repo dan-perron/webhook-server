27cc7833962d36436cae1997a1a702cebb9dc3d2
Dan Perron <github@djperron.com>
2020-04-10 10:10:53 -0500

    Give the server 5 sec to come up.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ docker build -t dan-perron/webhook-server .
 docker stop webhook-server
 docker rm webhook-server
 docker run  --name webhook-server --restart unless-stopped -p 3000:3000 -d dan-perron/webhook-server
+sleep 5
 curl -X POST -H "Content-Type: application/json" -d '{"text":"Deploy complete"}' http://localhost/webhooks/test
