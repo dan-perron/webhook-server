576d249d381085a5de561e367918a5b7a199bfa3
Dan Perron <github@djperron.com>
2020-04-16 22:49:10 -0500

    Error checking in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -e
+
 docker build -t dan-perron/webhook-server .
-docker stop webhook-server
-docker rm webhook-server
+docker stop webhook-server || true
+docker rm webhook-server || true
 docker run  --name webhook-server --restart unless-stopped -p 3000:3000 -d dan-perron/webhook-server
 sleep 5
 curl -X POST -H "Content-Type: application/json" -d '{"text":"Deploy complete"}' http://localhost/webhooks/test
