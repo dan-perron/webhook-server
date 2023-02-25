eb1bff8200bc9fe9286a3d935c2e33ff65d43b14
Dan Perron <djperron@djperron.com>
2023-02-24 20:29:36 -0600

    Prompt for highlights

--- bin/ootpFileManager.js
+++ bin/ootpFileManager.js
@@ -1,8 +1,9 @@
 const cheerio = require('cheerio');
 const chokidar = require('chokidar');
 const dayjs = require('dayjs');
+dayjs.extend(require('dayjs/plugin/isSameOrAfter'));
 const {watchFile} = require('node:fs');
-const {readFile} = require('node:fs/promises');
+const {readFile, readdir} = require('node:fs/promises');
 const {stat} = require('node:fs/promises');
 const {IncomingWebhook} = require('@slack/webhook');
 const config = require('config');
@@ -15,20 +16,30 @@ const teamToSlackMap = {
   'team_11': 'U6KNBPYLE',
   'team_13': 'U6CACS3GW',
   'team_20': 'U6AT12XSM',
-}
+};
 
 const fileToSlackMap = Object.fromEntries(
-    Object.entries(teamToSlackMap).map(([k, v]) => [`${k}.ootp`, v])
+    Object.entries(teamToSlackMap).map(([k, v]) => [`${k}.ootp`, v]),
 );
 const perronSlack = 'U6AT12XSM';
-const teams = ['Cincinnati Reds', 'Kansas City Royals', 'Miami Marlins', 'Oakland Athletics'];
+const teams = [
+  'Cincinnati Reds',
+  'Kansas City Royals',
+  'Miami Marlins',
+  'Oakland Athletics'];
 const injuryFileToSlackMap = Object.fromEntries(
-    Object.entries(teamToSlackMap).map(([k, v]) => [`/ootp/game/reports/html/teams/${k}_injuries.html`, v])
-);
+    Object.entries(teamToSlackMap).map(([k, v]) => [`/ootp/game/reports/html/teams/${k}_injuries.html`, v]));
 
 const pathToTeamUploads = '/ootp/game/team_uploads/';
 const pathToLeagueFile = '/ootp/game/league_file/cheeseburger_2023.zip';
 const pathToBoxScores = '/ootp/game/reports/html/box_scores/';
+const pathToHomePage = '/ootp/game/reports/html/leagues/league_202_home.html';
+
+async function getCurrentDate() {
+  const file = await readFile(pathToHomePage);
+  const cheer = cheerio.load(file);
+  return dayjs(cheer('div[style="text-align:center; color:#FFFFFF; padding-top:4px;"]').text());
+}
 
 for (const file in fileToSlackMap) {
   watchFile(pathToTeamUploads + file, async (curr, prev) => {
@@ -47,48 +58,85 @@ watchFile(pathToLeagueFile, () => {
   SlackWebhook.send({text: `New league file uploaded ${playersString}`});
 });
 
-async function boxScoreUpdate(path) {
+function matchTeamFilter(title, teamFilter) {
+  if (!teamFilter) {
+    return true;
+  }
+  if (Array.isArray(teamFilter)) {
+    for (let team of teams) {
+      if (title.includes(team)) {
+        return true;
+      }
+    }
+  }
+  return title.includes(teamFilter);
+}
+
+function matchDateFilter(date, dateFilter) {
+  if (!dateFilter) {
+    return true;
+  }
+  return date.isSameOrAfter(dateFilter);
+}
+
+async function getHighlightIfMatched(path, teamFilter, dateFilter) {
   const file = await readFile(path);
   const cheer = cheerio.load(file);
   const title = cheer('title').html();
-  for (let team of teams) {
-    if (title.includes(team)) {
-      const heading = cheer('td.boxtitle[style="padding:0px 4px 2px 4px;"]').text().trim();
-      const body = cheer('td.databg.datacolor[style="padding:1px 4px 2px 4px;"]').text().trim();
-      const url = path.replace('/ootp/game/reports/html', 'djperron.com/ootp');
-      SlackWebhook.send({
-        blocks: [
-          {
-            "type": "header",
-            "text": {
-              "type": "plain_text",
-              "text": heading
-            }
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": `<${url}|${title}>`
-            }
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "plain_text",
-              "text": body
-            }
-          }
-        ]
-      });
-      return;
-    }
+  if (!matchTeamFilter(title, teamFilter)) {
+    return null;
+  }
+  const date = dayjs(cheer('div[style="text-align:center; color:#FFFFFF; padding-top:4px;"]').text());
+  if (!matchDateFilter(date, dateFilter)) {
+    return null;
+  }
+  const heading = cheer('td.boxtitle[style="padding:0px 4px 2px 4px;"]').text().trim();
+  const body = cheer('td.databg.datacolor[style="padding:1px 4px 2px 4px;"]').text().trim();
+  const url = path.replace('/ootp/game/reports/html', 'djperron.com/ootp');
+  return {
+    blocks: [
+      {
+        'type': 'header',
+        'text': {
+          'type': 'plain_text',
+          'text': heading,
+        },
+      },
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': `<${url}|${title}>`,
+        },
+      },
+      {
+        'type': 'section',
+        'text': {
+          'type': 'plain_text',
+          'text': body,
+        },
+      },
+    ],
+  };
+}
+
+async function sendHighlights(path) {
+  const highlight = await getHighlightIfMatched(path, teams);
+  if (highlight) {
+    SlackWebhook.send(highlight);
   }
 }
 
-chokidar.watch(pathToBoxScores, {ignoreInitial: true})
-.on('add', boxScoreUpdate)
-.on('change', boxScoreUpdate);
+async function getHighlightsIfMatched(teamFilter, dateFilter) {
+  let files = await readdir(pathToBoxScores);
+  let highlights = files.map((f) => getHighlightIfMatched(pathToBoxScores + f, teamFilter, dateFilter)).
+      filter((h) => !!h);
+  return highlights;
+}
+
+chokidar.watch(pathToBoxScores, {ignoreInitial: true}).
+    on('add', sendHighlights).
+    on('change', sendHighlights);
 
 // for (const file in injuryFileToSlackMap) {
 //   chokidar.watch(file, {ignoreInitial: true}).on('change', async (path) => {
@@ -134,4 +182,4 @@ async function getBotMessage() {
   return getNextStepMessage(oldFiles);
 }
 
-module.exports = {getBotMessage};
+module.exports = {getBotMessage, getCurrentDate, getHighlightsIfMatched, teams};
--- bin/slackApi.js
+++ bin/slackApi.js
@@ -1,6 +1,6 @@
 const { App } = require('@slack/bolt');
 const config = require('config');
-const {getBotMessage} = require('./ootpFileManager');
+const {getBotMessage, getCurrentDate, teams, getHighlightsIfMatched} = require('./ootpFileManager');
 
 const app = new App({
   token: config.get('slack.token'),
@@ -12,12 +12,31 @@ const app = new App({
   // port: process.env.PORT || 3000
 });
 
+const highlightsChannel = 'C04J9TWRNJ3';
+
 app.message(/.*who.?s turn is it.*/i, async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
   console.log('⚡️ Msg recd! channel ' + message.channel);
-  if (message.channel === 'C04J9TWRNJ3') {
+  if (message.channel === highlightsChannel) {
     await say(await getBotMessage());
   }
+});
+
+const channelToTeam = {
+  'Cincinnati Reds':'C04NS45UKEX',
+  'Kansas City Royals':'C04NS44S6QK',
+  'Miami Marlins':'C04P4SB6LSD',
+  'Oakland Athletics':'C04NKK6CAKY',
+}
+
+app.message(/highlights please/i, async ({message, say}) => {
+  let teamFilter;
+  let dateFiler;
+  if (message.channel === highlightsChannel) {
+    teamFilter = teams;
+    dateFiler = getCurrentDate().subtract(1, 'days');
+  }
+  getHighlightsIfMatched(teamFilter, dateFiler).map((highlight) => say(highlight));
 });
 
 (async () => {
@@ -34,4 +53,3 @@ app.event('app_mention', async ({ event, say }) => {
 
 // testChannel - CUYGZ6LLU
 // ootp highlights - C04J9TWRNJ3
-
